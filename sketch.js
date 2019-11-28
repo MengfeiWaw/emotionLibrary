8ee680bbb700974081e1d34fd1205386a5b6abe8
MengfeiWaw <mwang@gsd.harvard.edu>
2019-11-27 19:39:45 -0500

    recording.html now use dat.gui

--- sketch.js
+++ sketch.js
@@ -7,6 +7,30 @@ let s2;
 let s3;
 let s4;
 
+let myData;
+let datGui;
+
+let MyData = function() {
+  this.message = 'hello';
+  this.value0 = 100;
+  this.value1 = 100;
+  this.value2 = 100;
+  this.value3 = 10;
+  this.displayOutline = false;
+};
+
+window.onload = function() {
+  myData = new MyData();
+  datGui = new dat.GUI();
+
+  datGui.add(myData, 'value0', 0, 255).listen();
+  datGui.add(myData, 'value1', 0, 255).listen();
+  datGui.add(myData, 'value2', 0, 255).listen();
+  datGui.add(myData, 'value3', 0, 20).listen();
+
+};
+
+
 function setup() {
     // createCanvas(windowWidth/1.5, windowHeight);
     createCanvas(1200, 900);
@@ -19,20 +43,20 @@ function setup() {
     img.size(windowWidth/1.5, windowHeight/1.5);
     img.hide();
 
-    gui = createGui();
-    // b = createButton("Record", 50, 160);
-    s1 = createSlider("Slider1", sliderXInitial, sliderY, sliderWidth, 32, 0, 255);
-    s2 = createSlider("Slider2", sliderXInitial+sliderWidth*1+sliderPad*2, sliderY, sliderWidth, 32, 0, 255);
-    s3 = createSlider("Slider3", sliderXInitial+sliderWidth*2+sliderPad*4, sliderY, sliderWidth, 32, 0, 255);
-    s4 = createSlider("Slider4", sliderXInitial+sliderWidth*3+sliderPad*6, sliderY, sliderWidth, 32, 0, 20);
-    s1.setStyle("fillBg", color(49,183,123));
-    s2.setStyle("fillBg", color(49,183,123));
-    s3.setStyle("fillBg", color(49,183,123));
-    s4.setStyle("fillBg", color(49,183,123));
-    s1.val = 100;
-    s2.val = 100;
-    s3.val = 100;
-    s4.val = 10;
+    // gui = createGui();
+    // // b = createButton("Record", 50, 160);
+    // s1 = createSlider("Slider1", sliderXInitial, sliderY, sliderWidth, 32, 0, 255);
+    // s2 = createSlider("Slider2", sliderXInitial+sliderWidth*1+sliderPad*2, sliderY, sliderWidth, 32, 0, 255);
+    // s3 = createSlider("Slider3", sliderXInitial+sliderWidth*2+sliderPad*4, sliderY, sliderWidth, 32, 0, 255);
+    // s4 = createSlider("Slider4", sliderXInitial+sliderWidth*3+sliderPad*6, sliderY, sliderWidth, 32, 0, 20);
+    // s1.setStyle("fillBg", color(49,183,123));
+    // s2.setStyle("fillBg", color(49,183,123));
+    // s3.setStyle("fillBg", color(49,183,123));
+    // s4.setStyle("fillBg", color(49,183,123));
+    // s1.val = 100;
+    // s2.val = 100;
+    // s3.val = 100;
+    // s4.val = 10;
 }
 
 function draw() {
@@ -44,20 +68,20 @@ function draw() {
       var p = img.pixels[(y*img.width-x)*4];
       if (p < 100 & p > 50 )
       {
-        fill(s1.val);
-		    rect(x, y+150, s4.val, s4.val);
+        fill(myData.value0);
+		    rect(x, y+150, myData.value3, myData.value3);
       }
       if (p < 200 & p > 100 )
       {
-        fill(p,205,s2.val);
-        rect(x, y+150, s4.val, s4.val);
+        fill(p,205,myData.value1);
+        rect(x, y+150, myData.value3, myData.value3);
       }
       if (p < 100 & p > 0 )
       {
-        fill(s3.val,97,p);
-        ellipse(x, y+150, s4.val, s4.val);
+        fill(myData.value2,97,p);
+        ellipse(x, y+150, myData.value3, myData.value3);
       }
     }
   }
-  drawGui();
+  //drawGui();
 }
